8781155de6388a4120033318ed6b96f6650cd63c
tyf <448371352@qq.com>
2020-12-31 00:12:42 +0800

    first commit

--- cron/help.js
+++ cron/help.js
@@ -3,20 +3,19 @@ function help(path) {
     url: path,
     method: 'GET',
   }
-  console.log("开始执行请求")
   $task.fetch(url).then((response) => {
     let data = response.body
     console.log(data)
     if (data.code == "200") {
       let title = `互助码`
       let subTitle = `互助码注册成功`
-      let detail = `互助码注册成功`
+      let detail = `${path}`
       console.log(`${title}, ${subTitle}, ${detail}`)
       $notify(title, subTitle, detail)
     } else {
         let title = `互助码`
         let subTitle = `互助码注册失败`
-        let detail = `互助码注册失败${path}`
+        let detail = `${path}`
         console.log(`${title}, ${subTitle}, ${detail}`)
         $notify(title, subTitle, detail)
     }
@@ -24,7 +23,7 @@ function help(path) {
   })
 }
 
-function doGet(){
+async function doGet(){
     let urlArr=[
         'https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/MDAKftK4zvbO_WALfpIG8qt9zd5YaBeE/',
         'https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/36w40xBAHmNTico_YRjLrQ/',
@@ -42,7 +41,7 @@ function doGet(){
         'http://api.turinglabs.net/api/v1/jd/pet/create/MTE1NDQ5OTIwMDAwMDAwNDMwNTczNDc=/'
     ];
     for(let i=0; i< urlArr.length; i++){
-        help(urlArr[i]);
+        await help(urlArr[i]);
     }
 }
 
